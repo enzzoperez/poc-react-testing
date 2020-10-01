4514f141d0bd105c3d494b74a027b3a6dd78cad1
Enzzo Perez <enzzorp@gmail.com>
2020-10-01 10:45:40 -0300

    testing for todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [taskToAdd, setTaskToAdd] = useState();
+    const [listTaks, setListTask] = useState([]);
+
+    const addTask = () => {
+        if (listTaks.length < 10) {
+            setTaskToAdd();
+            setListTask([...listTaks, taskToAdd]);
+        }
+    };
+
+    const deleteTask = (idTask) => {
+      const newListTask = listTaks.filter(e=>e.id != idTask)
+      setListTask(newListTask)
+    };
+
+    return (
+        <div>
+            <h1>Bienvenido a todo task</h1>
+            <label htmlFor="taskInput">Ingrese tarea</label>
+            <textarea
+                id={"taskInput"}
+                placeholder="Ingrese Tarea"
+                maxLength={100}
+                type="text"
+                value={(taskToAdd && taskToAdd.task) || ""}
+                onChange={(e) =>
+                    setTaskToAdd({
+                        id: new Date(Date.now()).toISOString(),
+                        task: e.target.value,
+                    })
+                }
+            />
+            <div>
+                <button
+                    disabled={!taskToAdd || taskToAdd.length < 1}
+                    onClick={() => addTask()}
+                >
+                    Agregar tarea
+                </button>
+            </div>
+            <div>
+                <h2>Tareas agregadas: {listTaks.length}</h2>
+                <div>
+                    {listTaks.length > 0 &&
+                        listTaks.map((t) => {
+                            return (
+                                <div key={t.id}>
+                                    <h3>{t.task}</h3>
+                                    <button onClick={() => deleteTask(t.id)}>
+                                        Eliminar
+                                    </button>
+                                </div>
+                            );
+                        })}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,72 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, cleanup, wait } from '@testing-library/react';
+import user from "@testing-library/user-event";
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const taskToAdd = 'one task'
+
+const renderToTest = () => {
+  const utils = render(<App />)
+    const inputTask = screen.getByPlaceholderText('ingrese', {exact: false})
+    const buttonAdd = screen.getByRole('button', {name: 'Agregar tarea'})
+    return {
+      utils,
+      inputTask,
+      buttonAdd
+    }
+}
+
+test('add a task', () => {
+    render(<App />)
+    const inputTask = screen.getByPlaceholderText('ingrese', {exact: false})
+    const buttonAdd = screen.getByRole('button', {name: 'Agregar tarea'})
+
+    expect(screen.getByText('ingrese', {exact: false})).toBeInTheDocument()
+    expect(buttonAdd).toBeDisabled()
+    
+    user.type(inputTask, taskToAdd)
+    expect(buttonAdd).toBeEnabled()
+
+    user.click(buttonAdd)
+    
+    expect(inputTask.textContent).toBe('')
+    expect(screen.getByText('tareas agregadas', {exact: false})).toHaveTextContent('1')
+})
+
+test('Testing doesnt allow more than 10 task', async() => {
+    render(<App />)
+    const inputTask = screen.getByPlaceholderText('ingrese', {exact: false})
+    const buttonAdd = screen.getByRole('button', {name: 'Agregar tarea'})
+    
+    await wait(()=>{
+      for (let index = 0; index < 11; index++) {
+        user.type(inputTask, `${index}a`)
+        expect(buttonAdd).toBeEnabled()
+        user.click(buttonAdd)
+      }
+    })
+
+    expect(screen.getByText('tareas agregadas', {exact: false})).toHaveTextContent('10')
+    
+    user.type(inputTask, `11 tarea`)
+    expect(buttonAdd).toBeEnabled()
+    user.click(buttonAdd)
+
+    expect(screen.getByText('tareas agregadas', {exact: false})).toHaveTextContent('10')
+})
+
+test('Test Delete Button', () => {
+  const {utils, buttonAdd, inputTask} = renderToTest()
+
+  user.type(inputTask, taskToAdd)
+  user.click(buttonAdd)
+
+  expect(utils.getByText('tareas agregadas', {exact: false})).toHaveTextContent('1')
+
+  const deleteButton = utils.getByRole('button', {name: 'Eliminar'})
+  user.click(deleteButton)
+
+  expect(utils.getByText('tareas agregadas', {exact: false})).toHaveTextContent('0')
+})
+
+
