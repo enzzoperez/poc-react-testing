78e77cf02b270fec8e854ed2ecd22f3cf232635f
Enzzo Perez <enzzorp@gmail.com>
2020-11-21 16:00:20 -0300

    add

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,38 @@
 import React from "react";
-import { render, screen, cleanup, wait } from "@testing-library/react";
+import {
+    render,
+    screen,
+    cleanup,
+    wait,
+} from "@testing-library/react";
 import user from "@testing-library/user-event";
 import App from "./App";
+import { getUser } from "./serviceGithub";
 
-
+const getUserSuccess = jest.mock(getUser);
+getUserSuccess.mockResolvedValue({
+    data: {
+        id: "2231231",
+        name: "enzouu",
+    },
+});
 
 const renderInit = () => {
     const utils = render(<App />);
     const inputUser = utils.getByPlaceholderText("ingrese usuario", {
         exact: false,
     });
-    const buttonSearch = utils.getByRole("button",{name: /buscar/i});
+    const buttonSearch = utils.getByRole("button", { name: /buscar/i });
 
-    return ({utils, buttonSearch, inputUser})
+    return { utils, buttonSearch, inputUser };
 };
 
-
-test('should success request to api', () => {
-    const {utils, buttonSearch, inputUser} = renderInit()
-    expect(utils.getByText(/esperando/i)).toBeInTheDocument()
-    expect(buttonSearch).toBeDisabled()
-    user.type('e', inputUser)
-    expect(buttonSearch).not.toBeDisabled()
-    // user.type('nzzoperez', inputUser)
-    // user.click(buttonSearch)
-
-})
+test("should success request to api", () => {
+    const { utils, buttonSearch, inputUser } = renderInit();
+    expect(utils.getByText(/esperando/i)).toBeInTheDocument();
+    expect(buttonSearch).toBeDisabled();
+    user.type(inputUser, "e");
+    expect(buttonSearch).toBeEnabled();
+    user.type(inputUser, "nzzoperez");
+    user.click(buttonSearch);
+});
